6cfef64e42369b5be4187ec6a47e4d28903fb0a9
amankv97 <ak2731165v@gmail.com>
2026-03-19 16:46:54 +0530

    Updated Project Properties

--- src/main/java/com/ecommerce/project/EcomHubApplication.java
+++ src/main/java/com/ecommerce/project/EcomHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvEcomApplication {
+public class EcomHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvEcomApplication.class, args);
+		SpringApplication.run(EcomHubApplication.class, args);
 	}
 
 }
